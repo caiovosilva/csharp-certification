efea9577129e5ed845a386d7cf9d5355a9afc276
Caio Silva <caiovosilva@outlook.com>
2019-10-23 18:53:39 -0300

    adicionando projeto

---
+++ types/certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/1 - Tipos de Valor/depois/TiposDeValor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class TiposDeValor : IAulaItem
+    {
+        public void Executar()
+        {
+            int intExample = 1;
+
+            Console.WriteLine(intExample);
+        }
+    }
+}
